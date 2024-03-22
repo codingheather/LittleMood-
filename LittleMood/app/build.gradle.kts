9b8679e69c45e34da24553b1a8ea61f8926af051
YingbingZhu <yingbing.zhu@hotmail.com>
2024-03-21 17:19:15 -0700

    feature: add mood in date

--- LittleMood/app/build.gradle.kts
+++ LittleMood/app/build.gradle.kts
@@ -32,7 +32,7 @@ android {
 }
 
 dependencies {
-
+    implementation("com.applandeo:material-calendar-view:1.9.0-rc04")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
