0d86e822fb31c8d0ec0fbcd4a9f114eebad6633c
Xiyu-Serena-Wan <64742214+Xiyu-Serena-Wan@users.noreply.github.com>
2024-03-25 00:10:12 -0700

    set up sign up

--- LittleMood/app/src/main/java/edu/northeastern/group26/littlemood/SignUpActivity.java
+++ LittleMood/app/src/main/java/edu/northeastern/group26/littlemood/SignUpActivity.java
@@ -1,6 +1,8 @@
 package edu.northeastern.group26.littlemood;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +12,9 @@ public class SignUpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+    }
+
+    public void signUp(View view) {
+        Toast.makeText(this, "Successfully signed up", Toast.LENGTH_SHORT).show();
     }
 }
