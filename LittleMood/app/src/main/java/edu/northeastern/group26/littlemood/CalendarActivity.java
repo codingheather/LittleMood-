4307da9b1e525a4fe1e0098069a8fee3a6d59a91
YingbingZhu <yingbing.zhu@hotmail.com>
2024-03-31 16:14:29 -0700

    add statistic layout and adjust calendar layout

--- LittleMood/app/src/main/java/edu/northeastern/group26/littlemood/CalendarActivity.java
+++ LittleMood/app/src/main/java/edu/northeastern/group26/littlemood/CalendarActivity.java
@@ -56,6 +56,7 @@ public class CalendarActivity extends AppCompatActivity {
     private ImageView addJournalButton;
     private ImageView searchIcon;
     private ImageView settingIcon;
+    private ImageView statisticIcon;
     private CalendarView calendarView;
     private SearchView searchInput;
     private TextView quoteTextView;
@@ -74,7 +75,7 @@ public class CalendarActivity extends AppCompatActivity {
         settingIcon = findViewById(R.id.settingsIcon);
         quoteTextView = findViewById(R.id.inspiringWordsTextView);
         searchInput = findViewById(R.id.searchView);
-
+        statisticIcon = findViewById(R.id.chartIcon);
         // get daily quote(inspiring words) from API and update UI
         getDailyQuote();
 
@@ -89,6 +90,13 @@ public class CalendarActivity extends AppCompatActivity {
             Intent intent = new Intent(this, SettingsActivity.class);
             startActivity(intent);
         });
+
+        // enter statistic page
+        statisticIcon.setOnClickListener(view -> {
+            Intent intent = new Intent(this, StatisticActivity.class);
+            startActivity(intent);
+        });
+
 
         // define search activity
         searchIcon = findViewById(R.id.searchIcon);
@@ -195,7 +203,7 @@ public class CalendarActivity extends AppCompatActivity {
     }
 
     private void showSearchedDate(SearchView searchView){
-        searchView.setQueryHint("Enter month (YYYY-MM)");
+        searchView.setQueryHint("YYYY-MM");
         if (searchView.getVisibility() == View.GONE) {
             searchView.setVisibility(View.VISIBLE);
         } else {
